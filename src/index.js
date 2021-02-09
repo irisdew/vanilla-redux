3a45fc801f27acb5ebf8ec976d573654dc9db501
isl <isl9702@naver.com>
2021-02-10 00:52:40 +0900

    1.4 Recap Redux Counter

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import {createStore} from "redux"; 
+import { createStore } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+//string으로 쓸 때보다 변수를 정의해두면 javascript가 정의되지 않은 변수라고 말해줘서 디버깅이 쉽다
+const ADD = "ADD" 
+const MINUS = "MINUS"
+
 const countModifier = (count = 0, action) => {
-    if (action.type === "ADD") {
-        return count + 1;
-    } else if (action.type === "MINUS") {
-        return count - 1;
-    } else {
-        return count;
+    switch (action.type) {
+        case ADD:
+            return count += 1;
+        case MINUS:
+            return count -= 1;
+        default:
+            return count;
     }
 };
 
 const countStore = createStore(countModifier);
 
-countStore.subscribe(()=>{ number.innerText = countStore.getState(); });
+const onChange = () => {
+    number.innerText = countStore.getState();
+};
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD"}));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS"}));
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
