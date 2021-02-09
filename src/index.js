e2ac4028e6bcc3efd2badd2968967cc54cd0d894
isl <isl9702@naver.com>
2021-02-10 00:46:02 +0900

    1.3 Redux Counter

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
+import {createStore} from "redux"; 
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-const updateText = () => {
-    number.innerText = count;
-}
-
-const handleAdd = () => {
-    count = count + 1;
-    updateText();
+const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 };
 
-const handleMinus = () => {
-    count = count - 1;
-    updateText();
-};
+const countStore = createStore(countModifier);
 
+countStore.subscribe(()=>{ number.innerText = countStore.getState(); });
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD"}));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS"}));
